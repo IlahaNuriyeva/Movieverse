86d6256ad54e955d61fcdf9df6bfb9b1d4fffb5c
Ilaha Nuriyeva <ilahenuriyeva000@gmail.com>
2022-04-09 23:08:53 +0400

    creating project

---
+++ src/main/java/az/developia/movieverse/MovieverseApplication.java
@@ -0,0 +1,13 @@
+package az.developia.movieverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieverseApplication.class, args);
+    }
+
+}
